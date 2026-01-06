f0100dc800e81d30d5f057723b0f3fb52fc2c629
adrienschuler <adrien.schuler@gmail.com>
2026-01-06 17:05:51 +0100

    feat(neetcode250): solved #2

---
+++ neetcode 250/arrays & hashing/02-217-contains-duplicate.py
@@ -0,0 +1,15 @@
+"""
+https://leetcode.com/problems/contains-duplicate/
+Easy
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+"""
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
