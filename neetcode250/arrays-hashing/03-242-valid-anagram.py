709cfd9407aef9779330be88718cebd98a327583
adrienschuler <adrien.schuler@gmail.com>
2026-01-06 20:32:27 +0100

    feat(neetcode250): solved #3

---
+++ neetcode250/arrays-hashing/03-242-valid-anagram.py
@@ -0,0 +1,27 @@
+"""
+https://leetcode.com/problems/valid-anagram
+Easy
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+"""
+
+# Approach: Frequency Counter
+# Time Complexity: O(n)
+# Space Complexity: O(1) - since the character set is limited
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        freq = defaultdict(int)
+
+        for char in s:
+            freq[char] += 1
+
+        for char in t:
+            if char in freq and freq[char] > 0:
+                freq[char] -= 1
+            else:
+                return False
+
+        return True
