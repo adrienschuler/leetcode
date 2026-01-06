c47a61b72c0ec6d076c04da0ef4aa48e974233d0
adrienschuler <adrien.schuler@gmail.com>
2026-01-06 20:31:54 +0100

    docs(neetcode250#2): added complexity

--- neetcode250/arrays-hashing/02-217-contains-duplicate.py
+++ neetcode250/arrays-hashing/02-217-contains-duplicate.py
@@ -5,6 +5,8 @@ Easy
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 """
 
+# Time complexity: O(n)
+# Space complexity: O(n)
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         seen = set()
