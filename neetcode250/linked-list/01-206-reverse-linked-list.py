b03cbff4f2930acf165eca776785d7c581db1f48
adrienschuler <adrien.schuler@gmail.com>
2026-01-19 21:47:28 +0100

    feat(nc/linked-list): mock #01

--- neetcode250/linked-list/01-206-reverse-linked-list.py
+++ neetcode250/linked-list/01-206-reverse-linked-list.py
@@ -7,19 +7,22 @@ Given the head of a singly linked list, reverse the list, and return the reverse
 # Topics: Linked List, Recursion
 
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
+# Approach: Iterative
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
-
-        while head:
-            current = head
-            head = head.next
-            current.next = prev
-            prev = current
-
+        prev, curr = None, head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
         return prev
+
+# TODO: recursive approach
