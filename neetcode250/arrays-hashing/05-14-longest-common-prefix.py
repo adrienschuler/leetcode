4c4be26ae8bf361a893eaf1a2c6696b5dd7b408f
adrienschuler <adrien.schuler@gmail.com>
2026-01-19 17:27:01 +0100

    feat(nc/arrays): mock #05 longest common prefix

--- neetcode250/arrays-hashing/05-14-longest-common-prefix.py
+++ neetcode250/arrays-hashing/05-14-longest-common-prefix.py
@@ -8,16 +8,33 @@ If there is no common prefix, return an empty string "".
 
 # Topics: Array, String
 
-# Approach: Vertical Scanning
-# Time Complexity: O(S) where S is the sum of all characters in all strings
+# Approach: Vertical Scanning with Shortest String
+# Time Complexity: O(m * n) where m is the length of the shortest string and n is the number of strings
 # Space Complexity: O(1)
+from typing import List
+
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         shortest = min(strs, key=len)
-
         for i, c in enumerate(shortest):
-            for string in strs:
-                if string[i] != c:
+            for s in strs:
+                if s[i] != c:
                     return shortest[:i]
-
         return shortest
+
+
+# Approach: Vertical Scanning
+# Time Complexity: O(m * n) where m is the length of the first string and n is the number of strings
+# Space Complexity: O(1)
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+
+        return res
