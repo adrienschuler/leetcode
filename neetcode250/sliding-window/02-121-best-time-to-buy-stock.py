519542248aeb85ab68b62acf363aeab3762b9ae6
adrienschuler <adrien.schuler@gmail.com>
2026-01-26 21:50:05 +0100

    feat(nc/sw): solved #02 best time to buy stock

---
+++ neetcode250/sliding-window/02-121-best-time-to-buy-stock.py
@@ -0,0 +1,30 @@
+"""
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+Easy
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+"""
+
+# Topics: Two Pointers
+
+# Approach: Sliding Window
+# Time Complexity: O(N) where N is the length of prices
+# Space Complexity: O(1)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                if profit > maxP:
+                    maxP = profit
+            else:
+                l = r
+            r += 1
+
+        return maxP
