ee3232b07cdbf48161e3afec281d067b2dcf7016
adrienschuler <adrien.schuler@gmail.com>
2026-03-06 16:51:06 +0100

    feat(heaps): top k sorting approachs

--- neetcode250/heap-priority-queue/692-top-k-frequent-words.py
+++ neetcode250/heap-priority-queue/692-top-k-frequent-words.py
@@ -9,9 +9,28 @@ Return the answer sorted by the frequency from highest to lowest. Sort the words
 # Topics: Hash Map, Heap (Priority Queue), Sorting
 
 
+# Approach: sort
+# Time:  O(n log n) where U is the number of unique words
+# Space: O(U) where U is the number of unique words
+class Solution:
+    def topKFrequent(words: list, k: int) -> List:
+        # Edge cases:
+        # - what if k > len(words)
+        # - ?
+
+        # 1. count the frequencies of each words
+        freqs = Counter(words)
+
+        # 2. ensure we sort also the lexicographical order
+        sorted_freqs = sorted(freqs.items(), key=lambda x: (-x[1], x[0]))
+
+        # 3. format the output
+        # 4. slice the top k frequencies
+        return [freqs[0] for freqs in sorted_freqs][:k]
+
 # Approach: Heap
-# Time Complexity: O(n log k)
-# Space Complexity: O(n)
+# Time Complexity: O(n log n) due to heap operations and sorting
+# Space Complexity: O(n) for the heap and frequency map
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         counts = Counter(words)
