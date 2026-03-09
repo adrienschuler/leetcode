1da387086f717450431b106959fb32c70938ec58
adrienschuler <adrien.schuler@gmail.com>
2026-03-09 21:42:39 +0100

    mocks

---
+++ neetcode250/arrays-hashing/14-longest-common-prefix.py
@@ -0,0 +1,36 @@
+"""
+14. Longest Common Prefix
+https://leetcode.com/problems/longest-common-prefix/
+Easy
+
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+"""
+
+# Topics: Array, String
+
+# Approach: Vertical Scanning with Shortest String
+# Time Complexity: O(m * n) where m is the length of the shortest string and n is the number of strings
+# Space Complexity: O(1)
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        shortest = min(strs, key=len)
+        for i, c in enumerate(shortest):
+            for s in strs:
+                if s[i] != c:
+                    return shortest[:i]
+        return shortest
+
+
+# Approach: Vertical Scanning
+# Time Complexity: O(m * n) where m is the length of the first string and n is the number of strings
+# Space Complexity: O(1)
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = ""
+        for i, char in enumerate(strs[0]):
+            for word in strs[1:]:
+                if len(word) == i or word[i] != char:
+                    return prefix
+            prefix += char
+        return prefix
